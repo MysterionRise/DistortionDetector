7e7335926bed0eae25671ee8dee6ca287cf8f781
Konstantin Perikov <pericov.ko@gmail.com>
2019-04-13 18:53:14 +0100

    add ui components

--- DistortionDetector/ViewController.swift
+++ DistortionDetector/ViewController.swift
@@ -9,13 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Properties
+    @IBOutlet weak var openCVLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("\(OpenCVWrapper.openCVVersionString())")
         // Do any additional setup after loading the view.
     }
 
+    //MARK: Actions
+    @IBAction func checkOpenCVVersion(_ sender: Any) {
+        openCVLabel?.text = OpenCVWrapper.openCVVersionString()
+    }
+    
 
 }
 
